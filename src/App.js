ec3f7e03c7f47018d96fa02e659724a7e09d6f0b
Sina <sinarahimi.main@example.com>
2022-05-31 18:33:06 +0430

    Creating user interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
+
+  const [result, setResult] = useState("Not retrieved yet!");
+  const [consecutiveWins, setConsecutiveWins] = useState("not fetched yet!")
+
+  const handleDecide = () => {
+    console.log("Retrieving data from my contract!");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Carbonyte Project</header>
+      <div>
+        <div className="row">
+
+          <div className="column">
+            <h4>My Helper Contract</h4>
+            <h6>Result: {result}</h6>
+            <button className="button" onClick={handleDecide}>Decide</button>
+          </div>
+
+          <div className="vl"></div>
+
+          <div className="column">
+            <h4>Your Arbitrum Contract</h4>
+            <h6>Consecutive Wins: {consecutiveWins}</h6>
+            <button className="button">Send True</button>
+            <button className="button">Send False</button>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
