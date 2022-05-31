51cda13c2341c025f8e535cc8469d17779be62dc
Sina <sinarahimi.main@example.com>
2022-05-31 23:15:09 +0430

    last moments before success

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from "react";
+import ArbitrumABI from "./ArbitrumABI.json";
+import MyContractABI from "./MyContractABI.json";
+import {ethers, BigNumber} from "ethers";
+
+
 
 
 function App() {
 
-  const [result, setResult] = useState("Not retrieved yet!");
-  const [consecutiveWins, setConsecutiveWins] = useState("not fetched yet!")
+  const contractAddress = "0x18B7DFF2a1EC88790D129719232FC07e50FC29A5";
+  const myContract = "0x07E3DFD2A4CCAc8b857fEC1477CAEa10A91fF17E";
 
-  const handleDecide = () => {
-    console.log("Retrieving data from my contract!");
+  const [wins, setWins] = useState("not fetched yet!");
+
+  const handleDecide = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const instance = new ethers.Contract(myContract, MyContractABI.abi, signer);
+    const response = await instance.decide();
+    console.log(response);
   }
+
+  const handleWalletConnect = async () => {
+    if (window.ethereum) {
+      window.ethereum.request({ method: "eth_requestAccounts" }).then(result => console.log("Your Public Key:",result[0]));
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const instance = new ethers.Contract(contractAddress, ArbitrumABI.abi, provider);
+
+      const response = await instance.consecutiveWins();
+      setWins(ethers.BigNumber.from(response).toNumber());
+
+    } else {
+      alert("Need to install Metamask!");
+    }
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">Carbonyte Project</header>
+
+      <header className="App-header">Carbonyte Project
+        <button className="button" onClick={handleWalletConnect}>Update Data</button>
+      </header>
+      
       <div>
         <div className="row">
 
           <div className="column">
             <h4>My Helper Contract</h4>
-            <h6>Result: {result}</h6>
-            <button className="button" onClick={handleDecide}>Decide</button>
+            <button className="button" onClick={handleDecide}>Execute</button>
           </div>
 
           <div className="vl"></div>
 
           <div className="column">
             <h4>Your Arbitrum Contract</h4>
-            <h6>Consecutive Wins: {consecutiveWins}</h6>
-            <button className="button">Send True</button>
-            <button className="button">Send False</button>
+            <h6>Consecutive Wins: {wins}</h6>
           </div>
 
         </div>
       </div>
+
     </div>
   );
 }
